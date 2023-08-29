efbc0f11cf5cc69d959ab498f04189e05c6a98b6
Nut Jiraporn <nut.j@finnomena.com>
2023-08-29 16:00:19 +0700

    Initial Commit

---
+++ BuildConfigurationsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuildConfigurationsDemo
+//
+//  Created by Nut Jiraporn on 29/8/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
